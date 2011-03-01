53d3e1acba7f71eb08428a8a6c8c0f2fe7594ca0
Hiroshi Hashiguchi <xcatsan@mac.com>
2011-03-02 08:14:17 +0900

    Initial Commit

---
+++ FBFileCache/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FBFileCache
+//
+//  Created by 橋口 湖 on 11/03/02.
+//  Copyright 2011 ファイブテクノロジー株式会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ FBFileCacheTests/FBFileCacheTests.h
@@ -0,0 +1,17 @@
+//
+//  FBFileCacheTests.h
+//  FBFileCacheTests
+//
+//  Created by 橋口 湖 on 11/03/02.
+//  Copyright 2011 ファイブテクノロジー株式会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface FBFileCacheTests : SenTestCase {
+@private
+    
+}
+
+@end
